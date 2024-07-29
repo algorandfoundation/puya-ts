887b8140f0723c780d431838495b77b4c380d1b1
Bobby Lat <bobby.lat@makerx.com.au>
2024-07-29 10:21:32 +0800

    upcast all numeric values to bigint before evaluating any binary ops to allow for expressions such as `Uint64(2 ** 64 - 1)` to get largest valid uint64 value.

--- packages/algo-ts-testing/src/runtime-helpers.ts
+++ packages/algo-ts-testing/src/runtime-helpers.ts
@@ -1,7 +1,7 @@
+import { internalError } from './errors'
 import { AlgoTsPrimitiveCls, BigUintCls, Uint64Cls } from './primitives'
-import { codeError, internalError } from './errors'
-import { nameOfType } from './util'
 import { DeliberateAny } from './typescript-helpers'
+import { nameOfType } from './util'
 export { attachAbiMetadata } from './abi-metadata'
 export function switchableValue(x: unknown): bigint | string | boolean {
   if (typeof x === 'boolean') return x
@@ -19,71 +19,6 @@ export function switchableValue(x: unknown): bigint | string | boolean {
 
 type BinaryOps = '+' | '-' | '*' | '**' | '/' | '>' | '>=' | '<' | '<=' | '===' | '!==' | '<<' | '>>'
 
-function uint64BinaryOp(left: bigint, right: bigint, op: BinaryOps) {
-  switch (op) {
-    case '+':
-      return new Uint64Cls(left + right)
-    case '-':
-      return new Uint64Cls(left - right)
-    case '*':
-      return new Uint64Cls(left * right)
-    case '**':
-      return new Uint64Cls(left ** right)
-    case '/':
-      return new Uint64Cls(left / right)
-    case '>>':
-      return new Uint64Cls(left >> right)
-    case '<<':
-      return new Uint64Cls(left << right)
-    case '>':
-      return left > right
-    case '<':
-      return left < right
-    case '>=':
-      return left >= right
-    case '<=':
-      return left <= right
-    case '===':
-      return left === right
-    case '!==':
-      return left !== right
-    default:
-      codeError(`Unsupported operator ${op}`)
-  }
-}
-function biguintBinaryOp(left: bigint, right: bigint, op: BinaryOps) {
-  switch (op) {
-    case '+':
-      return new BigUintCls(left + right)
-    case '-':
-      return new BigUintCls(left - right)
-    case '*':
-      return new BigUintCls(left * right)
-    case '**':
-      return new BigUintCls(left ** right)
-    case '/':
-      return new BigUintCls(left / right)
-    case '>>':
-      return new BigUintCls(left >> right)
-    case '<<':
-      return new BigUintCls(left << right)
-    case '>':
-      return left > right
-    case '<':
-      return left < right
-    case '>=':
-      return left >= right
-    case '<=':
-      return left <= right
-    case '===':
-      return left === right
-    case '!==':
-      return left !== right
-    default:
-      codeError(`Unsupported operator ${op}`)
-  }
-}
-
 function tryGetBigInt(value: unknown): bigint | undefined {
   if (typeof value == 'bigint') return value
   if (typeof value == 'number') return BigInt(value)
@@ -93,29 +28,16 @@ function tryGetBigInt(value: unknown): bigint | undefined {
 }
 
 export function binaryOp(left: unknown, right: unknown, op: BinaryOps) {
-  if (left instanceof BigUintCls) {
-    const rbi = tryGetBigInt(right)
-    if (rbi !== undefined) {
-      return biguintBinaryOp(left.valueOf(), rbi, op)
+  const lbi = tryGetBigInt(left)
+  const rbi = tryGetBigInt(right)
+  if (lbi !== undefined && rbi !== undefined) {
+    const result =  defaultBinaryOp(lbi, rbi, op)
+    if (left instanceof BigUintCls || right instanceof BigUintCls) {
+      return new BigUintCls(result)
+    } else if (left instanceof Uint64Cls || right instanceof Uint64Cls) {
+      return new Uint64Cls(result)
     }
-  }
-  if (right instanceof BigUintCls) {
-    const lbi = tryGetBigInt(right)
-    if (lbi !== undefined) {
-      return biguintBinaryOp(lbi, right.valueOf(), op)
-    }
-  }
-  if (left instanceof Uint64Cls) {
-    const rbi = tryGetBigInt(right)
-    if (rbi !== undefined) {
-      return uint64BinaryOp(left.valueOf(), rbi, op)
-    }
-  }
-  if (right instanceof Uint64Cls) {
-    const lbi = tryGetBigInt(right)
-    if (lbi !== undefined) {
-      return uint64BinaryOp(lbi, right.valueOf(), op)
-    }
+    return result
   }
   return defaultBinaryOp(left, right, op)
 }
