778ea223aa37cc23aa2bc7360440b007bf7ee2be
Bobby Lat <bobby.lat@makerx.com.au>
2024-08-14 17:20:29 +0800

    rename executeInScope to createExecutionScope to be inline with python

--- packages/algo-ts-testing/src/subcontexts/transaction-context.ts
+++ packages/algo-ts-testing/src/subcontexts/transaction-context.ts
@@ -1,7 +1,6 @@
 import { gtxn, internal } from '@algorandfoundation/algo-ts'
-import { DeliberateAny } from '../typescript-helpers'
 
-function Scope(dispose: () => void) {
+function ScopeGenerator(dispose: () => void) {
   function* internal() {
     try {
       yield
@@ -18,34 +17,42 @@ function Scope(dispose: () => void) {
   }
 }
 
+interface ExecutionScope {
+  execute: <TReturn>(body: () => TReturn) => TReturn
+}
+
 export class TransactionContext {
   #groups: TransactionGroup[] = []
   #activeGroup: TransactionGroup | undefined
 
-  executeInScope(group: gtxn.Transaction[], activeTransactionIndex?: number): (body: () => DeliberateAny) => DeliberateAny {
+  createExecutionScope(group: gtxn.Transaction[], activeTransactionIndex?: number): ExecutionScope {
     const transactionGroup = new TransactionGroup(group, activeTransactionIndex)
     const previousGroup = this.#activeGroup
     this.#activeGroup = transactionGroup
 
-    const scope = Scope(() => {
+    const scope = ScopeGenerator(() => {
       if (this.#activeGroup?.transactions?.length) {
         this.#groups.push(this.#activeGroup)
       }
       this.#activeGroup = previousGroup
     })
-    return (body: () => DeliberateAny) => {
-      const result = body()
-      scope.done()
-      return result
+    return {
+      execute: <TReturn>(body: () => TReturn) => {
+        const result = body()
+        scope.done()
+        return result
+      },
     }
   }
 
-  tryExecuteInActiveScope(group: gtxn.Transaction[], activeTransactionIndex?: number): (body: () => DeliberateAny) => DeliberateAny {
+  ensureExecutionScope(group: gtxn.Transaction[], activeTransactionIndex?: number): ExecutionScope {
     if (!this.#activeGroup || !this.#activeGroup.transactions.length) {
-      return this.executeInScope(group, activeTransactionIndex)
+      return this.createExecutionScope(group, activeTransactionIndex)
     }
-    return (body: () => DeliberateAny) => {
-      return body()
+    return {
+      execute: <TReturn>(body: () => TReturn) => {
+        return body()
+      },
     }
   }
 
