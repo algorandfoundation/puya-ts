cdce2107767039c0151afcdc6add57ed73f3faf7
Tristan Menzel <tristanmenzel@gmail.com>
2024-10-08 16:53:47 -0700

    feat: Export encoding util methods

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,38 @@ import { logger } from './logger'
 import { createTsProgram } from './parser'
 import { invokePuya } from './puya'
 import type { PuyaPassThroughOptions } from './puya/options'
+import {
+  base32ToUint8Array,
+  base64ToUint8Array,
+  bigIntToUint8Array,
+  hexToUint8Array,
+  uint8ArrayToBase32,
+  uint8ArrayToBase64,
+  uint8ArrayToBigInt,
+  uint8ArrayToHex,
+  uint8ArrayToUtf8,
+  utf8ToUint8Array,
+} from './util'
 
 export { SourceLocation } from './awst/source-location'
 export { anyPType, ContractClassPType, FunctionPType, PType } from './awst_build/ptypes'
 export { registerPTypes } from './awst_build/ptypes/register'
 export { typeRegistry } from './awst_build/type-registry'
 export { TypeResolver } from './awst_build/type-resolver'
+
+export const encodingUtil = {
+  utf8ToUint8Array,
+  bigIntToUint8Array,
+  hexToUint8Array,
+  base32ToUint8Array,
+  base64ToUint8Array,
+
+  uint8ArrayToUtf8,
+  uint8ArrayToHex,
+  uint8ArrayToBase32,
+  uint8ArrayToBase64,
+  uint8ArrayToBigInt,
+}
 
 export type CompileResult = {
   logs: LogEvent[]
