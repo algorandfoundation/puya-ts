daf87de7aae14e1d06b35238d1b58a71f36ea333
Tristan Menzel <tristanmenzel@gmail.com>
2024-10-16 09:33:20 -0700

    fix: Export paths for algorand-typescript package

--- tests/approvals/intrinsic-calls.algo.ts
+++ tests/approvals/intrinsic-calls.algo.ts
@@ -1,7 +1,11 @@
 import type { bytes, uint64 } from '@algorandfoundation/algorand-typescript'
 import { op } from '@algorandfoundation/algorand-typescript'
+import { itob, sha256 } from '@algorandfoundation/algorand-typescript/op'
 
 function test(a: uint64, b: bytes) {
   const x = op.setBit(a, 8, 1)
   const y = op.setBit(b, 12, 9)
+
+  const z = itob(x)
+  const g = sha256(z)
 }
