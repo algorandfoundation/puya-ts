d7f0fd5bc78f9cc316efb9b45f4876bd4b958933
Tristan Menzel <tristanmenzel@gmail.com>
2024-11-01 10:49:34 -0700

    fix: Importing consts from external files

--- tests/approvals/inheritance-b.algo.ts
+++ tests/approvals/inheritance-b.algo.ts
@@ -1,13 +1,18 @@
 import type { uint64 } from '@algorandfoundation/algorand-typescript'
-import { op, Txn } from '@algorandfoundation/algorand-typescript'
-import { Arc4Contract, SimpleContract } from './inheritance-a.algo'
+import { log, op, Txn } from '@algorandfoundation/algorand-typescript'
+import { Arc4Contract, SimpleContract, VERY_IMPORTANT_VALUE } from './inheritance-a.algo'
 
 export class ConcreteSimpleContract extends SimpleContract {
   public approvalProgram(): uint64 {
     const a = op.btoi(Txn.applicationArgs(0))
     const b = op.btoi(Txn.applicationArgs(1))
-    return this.simpleMethod(a, b)
+    log(this.simpleMethod(a, b))
+    return 1
   }
 }
 
-export class ConcreteArc4Contract extends Arc4Contract {}
+export class ConcreteArc4Contract extends Arc4Contract {
+  public getVeryImportantValue() {
+    return VERY_IMPORTANT_VALUE
+  }
+}
---
+++ tests/onchain/util/decode-logs.ts
@@ -0,0 +1,27 @@
+import type { SendAppTransactionResult } from '@algorandfoundation/algokit-utils/types/app'
+import { uint8ArrayToBigInt, uint8ArrayToUtf8 } from '../../../src/util'
+
+export type LogDecoding = 'i' | 's' | 'b'
+
+export type DecodedLog<T extends LogDecoding> = T extends 'i' ? bigint : T extends 's' ? string : Uint8Array
+export type DecodedLogs<T extends [...LogDecoding[]]> = {
+  [Index in keyof T]: DecodedLog<T[Index]>
+} & { length: T['length'] }
+
+export function decodeLogs<const T extends [...LogDecoding[]]>(txnResult: SendAppTransactionResult, decoding: T): DecodedLogs<T> {
+  return decoding.map((decoding, i) => {
+    const log = txnResult.confirmation.logs?.[i]
+    if (log === undefined) {
+      throw new Error(`Cannot find logs log ${i}`)
+    }
+
+    switch (decoding[i]) {
+      case 'i':
+        return uint8ArrayToBigInt(log)
+      case 's':
+        return uint8ArrayToUtf8(log)
+      default:
+        return log
+    }
+  }) as DecodedLogs<T>
+}
