8dff16304b744c4543b18469dac98c44a8f2dafe
Tristan Menzel <tristan.menzel@makerx.com.au>
2024-12-12 14:28:25 +1000

    test: Add simple test scenario for assert match

--- tests/approvals/assert-match.algo.ts
+++ tests/approvals/assert-match.algo.ts
@@ -1,26 +1,16 @@
-import { assertMatch, BaseContract, Global, gtxn, Txn } from '@algorandfoundation/algorand-typescript'
+import type { gtxn } from '@algorandfoundation/algorand-typescript'
+import { assertMatch, Contract, Global, Txn } from '@algorandfoundation/algorand-typescript'
 
-function testSelf() {
-  assertMatch(Txn, {
-    receiver: Global.currentApplicationAddress,
-  })
-}
-
-function test(x: gtxn.PaymentTxn) {
-  assertMatch(x, {
-    amount: { between: [0, 50000] },
-    sender: Txn.sender,
-    receiver: Global.currentApplicationAddress,
-    closeRemainderTo: Global.zeroAddress,
-    firstValid: { greaterThan: 1 },
-    lastValid: { lessThan: 2 ** 40 },
-  })
-}
-
-export class AssertMatchContract extends BaseContract {
-  public approvalProgram(): boolean {
-    const txn = gtxn.PaymentTxn(1)
-    test(txn)
+export class AssertMatchContract extends Contract {
+  public testPay(pay: gtxn.PaymentTxn): boolean {
+    assertMatch(pay, {
+      amount: { between: [0, 50000] },
+      sender: Txn.sender,
+      receiver: Global.currentApplicationAddress,
+      closeRemainderTo: Global.zeroAddress,
+      firstValid: { greaterThan: 1 },
+      lastValid: { lessThan: 2 ** 40 },
+    })
     return true
   }
 }
