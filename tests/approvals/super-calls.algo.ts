3cfc4d9b617b4c97ae637d868d276904aa536894
Tristan Menzel <tristan.menzel@makerx.com.au>
2024-12-13 15:36:01 +1000

    perf: Don't generate superfluous constructors in AWST

---
+++ tests/approvals/super-calls.algo.ts
@@ -0,0 +1,43 @@
+import type { uint64 } from '@algorandfoundation/algorand-typescript'
+import { assert, BaseContract, GlobalState, Uint64 } from '@algorandfoundation/algorand-typescript'
+
+export class SuperContract extends BaseContract {
+  g1 = GlobalState({ initialValue: Uint64(1) })
+
+  approvalProgram(): boolean {
+    assert(this.g1.value === 1)
+    return true
+  }
+}
+
+export class SubContract extends SuperContract {
+  g2 = GlobalState({ initialValue: Uint64(2) })
+
+  approvalProgram(): boolean {
+    assert(this.g1.value === 1)
+    assert(this.g2.value === 2)
+    return true
+  }
+}
+
+export class SubSubContract extends SubContract {
+  approvalProgram(): boolean {
+    assert(this.g1.value === 1)
+    assert(this.g2.value === 2)
+    return true
+  }
+}
+
+export class SubSubSubContract extends SubSubContract {
+  g3 = GlobalState<uint64>()
+  constructor() {
+    super()
+    this.g3.value = 3
+  }
+  approvalProgram(): boolean {
+    assert(this.g1.value === 1)
+    assert(this.g2.value === 2)
+    assert(this.g3.value === 3)
+    return true
+  }
+}
