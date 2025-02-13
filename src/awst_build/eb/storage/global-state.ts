2f7dfcb4de01c98b4f1af1e92dbcba640e46d64d
Tristan Menzel <tristanmenzel@gmail.com>
2025-02-12 16:35:45 -0800

    test: Expand test coverage of global state proxy and resolve issues

--- src/awst_build/eb/storage/global-state.ts
+++ src/awst_build/eb/storage/global-state.ts
@@ -1,10 +1,8 @@
 import { nodeFactory } from '../../../awst/node-factory'
-import type { AppStateExpression, Expression, LValue } from '../../../awst/nodes'
+import type { AppStateExpression, Expression } from '../../../awst/nodes'
 import { BytesConstant } from '../../../awst/nodes'
 import type { SourceLocation } from '../../../awst/source-location'
 import { wtypes } from '../../../awst/wtypes'
-
-import { CodeError } from '../../../errors'
 import { codeInvariant, invariant } from '../../../util'
 import { AppStorageDeclaration } from '../../models/app-storage-declaration'
 import type { ContractClassPType, PType } from '../../ptypes'
@@ -12,7 +10,7 @@ import { boolPType, bytesPType, GlobalStateType, stringPType } from '../../ptype
 import { typeRegistry } from '../../type-registry'
 import { BooleanExpressionBuilder } from '../boolean-expression-builder'
 import type { NodeBuilder } from '../index'
-import { FunctionBuilder, InstanceBuilder, InstanceExpressionBuilder } from '../index'
+import { FunctionBuilder, InstanceExpressionBuilder } from '../index'
 import { parseFunctionArgs } from '../util/arg-parsing'
 import { VoidExpressionBuilder } from '../void-expression-builder'
 import { extractKey } from './util'
@@ -99,7 +97,7 @@ export class GlobalStateExpressionBuilder extends InstanceExpressionBuilder<Glob
     return super.memberAccess(name, sourceLocation)
   }
 
-  private buildField(): AppStateExpression {
+  protected buildField(): AppStateExpression {
     return nodeFactory.appStateExpression({
       key: this._expr,
       wtype: this.ptype.contentType.wtypeOrThrow,
@@ -109,33 +107,35 @@ export class GlobalStateExpressionBuilder extends InstanceExpressionBuilder<Glob
   }
 }
 
-export class GlobalStateFunctionResultBuilder extends InstanceBuilder<GlobalStateType> {
+export class GlobalStateFunctionResultBuilder extends GlobalStateExpressionBuilder {
   resolve(): Expression {
     codeInvariant(
-      this._expr,
+      this._keyExpr,
       'Global state must have explicit key provided if not being assigned to a contract property',
       this.sourceLocation,
     )
+    codeInvariant(!this.initialValue, 'Global state can only have an initial value specified if being assigned to a contract property')
     return this._expr
   }
-  resolveLValue(): LValue {
-    throw CodeError.invalidAssignmentTarget({ name: this.typeDescription, sourceLocation: this.sourceLocation })
-  }
-  private _ptype: GlobalStateType
-  private _expr: Expression | undefined
   public readonly initialValue: Expression | undefined
+
+  private readonly _keyExpr: Expression | undefined
+
   constructor(expr: Expression | undefined, ptype: PType, config: { initialValue?: Expression; sourceLocation: SourceLocation }) {
     const sourceLocation = expr?.sourceLocation ?? config?.sourceLocation
     invariant(sourceLocation, 'Must have expression or config')
-    super(sourceLocation)
-    invariant(ptype instanceof GlobalStateType, 'ptype must be GlobalStateType')
-    this._ptype = ptype
-    this._expr = expr
+    super(expr ?? nodeFactory.voidConstant({ sourceLocation }), ptype)
     this.initialValue = config.initialValue
+    this._keyExpr = expr
   }
 
-  get ptype(): GlobalStateType {
-    return this._ptype
+  protected buildField(): AppStateExpression {
+    codeInvariant(
+      this._keyExpr,
+      'Global state must have explicit key provided if not being assigned to a contract property',
+      this.sourceLocation,
+    )
+    return super.buildField()
   }
 
   buildStorageDeclaration(
@@ -144,16 +144,16 @@ export class GlobalStateFunctionResultBuilder extends InstanceBuilder<GlobalStat
     memberDescription: string | null,
     contractType: ContractClassPType,
   ): AppStorageDeclaration {
-    if (this._expr)
+    if (this._keyExpr)
       codeInvariant(
-        this._expr instanceof BytesConstant,
+        this._keyExpr instanceof BytesConstant,
         `key must be a compile time constant value if ${this.typeDescription} is assigned to a contract member`,
       )
     return new AppStorageDeclaration({
       sourceLocation: memberLocation,
-      ptype: this._ptype,
+      ptype: this.ptype,
       memberName: memberName,
-      keyOverride: this._expr ?? null,
+      keyOverride: this._keyExpr ?? null,
       description: memberDescription,
       definedIn: contractType,
     })
--- tests/expected-output/global-state.algo.ts
+++ tests/expected-output/global-state.algo.ts
@@ -1,6 +1,20 @@
-import { Contract, GlobalState } from '@algorandfoundation/algorand-typescript'
+import { Bytes, Contract, GlobalState, type uint64 } from '@algorandfoundation/algorand-typescript'
 
 export class TestContract extends Contract {
   // @expect-error unknown is not a valid type for storage
   noInitial = GlobalState()
+
+  // @expect-error Global state must have explicit key provided if not being assigned to a contract property
+  incorrect = GlobalState<string>().hasValue
+
+  // @expect-error Unsupported property type boolean. Only GlobalState, LocalState, and Box proxies can be stored on a contract.
+  incorrect2 = GlobalState<string>({ key: 'abc' }).hasValue
+
+  test() {
+    // @expect-error Global state must have explicit key provided if not being assigned to a contract property
+    const proxyNoKey = GlobalState<uint64>({})
+
+    // @expect-error Global state can only have an initial value specified if being assigned to a contract property
+    const proxyInvalidInitialValue = GlobalState<uint64>({ key: Bytes('abc'), initialValue: 1 })
+  }
 }
