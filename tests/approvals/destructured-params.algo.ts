51c1c7c9c7fb64d338c9e4588f8a900010b361e6
Tristan Menzel <tristanmenzel@gmail.com>
2025-02-14 12:19:01 -0800

    test: Array (tuple) and parameter destructuring

--- tests/approvals/destructured-params.algo.ts
+++ tests/approvals/destructured-params.algo.ts
@@ -1,12 +1,14 @@
 import type { bytes, uint64 } from '@algorandfoundation/algorand-typescript'
-import { Bytes, log, Uint64 } from '@algorandfoundation/algorand-typescript'
+import { Bytes, Contract, log, Uint64 } from '@algorandfoundation/algorand-typescript'
 
-function test({ a, b, c }: { a: uint64; b: bytes; c: boolean }): void {
-  log(a, b, c)
-}
+export class DestructuredParamsAlgo extends Contract {
+  test({ a, b, c }: { a: uint64; b: bytes; c: boolean }): void {
+    log(a, b, c)
+  }
 
-function init() {
-  test({ a: 1, b: Bytes(''), c: false })
-  const temp = { a: Uint64(2), b: Bytes('Hello'), c: true }
-  test(temp)
+  init() {
+    this.test({ a: 456, b: Bytes(''), c: false })
+    const temp = { a: Uint64(2), b: Bytes('Hello'), c: true }
+    this.test(temp)
+  }
 }
