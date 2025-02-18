f2959c40aebda320690136c101aec8499d0f5bb8
Tristan Menzel <tristanmenzel@gmail.com>
2025-02-18 14:48:18 -0800

    refactor: Remove circular dependency

--- src/awst_build/eb/util/index.ts
+++ src/awst_build/eb/util/index.ts
@@ -1,12 +1,11 @@
-import type { awst, ConstantValue } from '../../../awst'
+import type { awst } from '../../../awst'
 import { isConstant } from '../../../awst'
 import type { Expression } from '../../../awst/nodes'
 import { BoolConstant, IntegerConstant, StringConstant } from '../../../awst/nodes'
 import { CodeError } from '../../../errors'
 import { codeInvariant } from '../../../util'
 import type { PType, PTypeOrClass } from '../../ptypes'
-import { bigIntPType, biguintPType, boolPType, bytesPType, numberPType, stringPType, uint64PType } from '../../ptypes'
-import { UFixedNxMType, UintNType } from '../../ptypes/arc4-types'
+import { biguintPType, boolPType, stringPType, uint64PType } from '../../ptypes'
 import type { NodeBuilder } from '../index'
 import { InstanceBuilder } from '../index'
 import { LiteralExpressionBuilder } from '../literal-expression-builder'
@@ -104,34 +103,4 @@ export function requireConstantOfType(builder: NodeBuilder, ptype: PType, messag
 export function requireLiteralNumber(builder: NodeBuilder) {
   codeInvariant(builder instanceof BigIntLiteralExpressionBuilder, 'Expected numeric literal', builder.sourceLocation)
   return builder.value
-}
-
-export function isValidLiteralForPType(literalValue: ConstantValue, ptype: PTypeOrClass): boolean {
-  if (ptype.equals(stringPType)) {
-    return typeof literalValue === 'string'
-  }
-  if (ptype.equals(numberPType)) {
-    return (
-      typeof literalValue === 'bigint' && BigInt(Number.MIN_SAFE_INTEGER) <= literalValue && literalValue <= BigInt(Number.MAX_SAFE_INTEGER)
-    )
-  }
-  if (ptype.equals(bigIntPType)) {
-    return typeof literalValue === 'bigint'
-  }
-  if (ptype.equals(uint64PType)) {
-    return typeof literalValue === 'bigint' && 0 <= literalValue && literalValue < 2n ** 64n
-  }
-  if (ptype.equals(biguintPType)) {
-    return typeof literalValue === 'bigint' && 0 <= literalValue && literalValue < 2n ** 512n
-  }
-  if (ptype instanceof UintNType || ptype instanceof UFixedNxMType) {
-    return typeof literalValue === 'bigint' && 0 <= literalValue && literalValue < 2n ** ptype.n
-  }
-  if (ptype.equals(boolPType)) {
-    return typeof literalValue === 'boolean'
-  }
-  if (ptype.equals(bytesPType)) {
-    return literalValue instanceof Uint8Array
-  }
-  return false
 }
