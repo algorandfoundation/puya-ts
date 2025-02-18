cab5b8c45a5e202986bc8ea8c06b3290002976fa
Tristan Menzel <tristanmenzel@gmail.com>
2025-02-18 11:35:12 -0800

    chore: Bump puya dep

--- src/constants.ts
+++ src/constants.ts
@@ -33,7 +33,7 @@ export const Constants = {
   encodedAddressLength: 58,
   zeroAddressEncoded: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY5HFKQ',
   supportedAvmVersions: [10n, 11n],
-  targetedPuyaVersion: '4.3.3',
+  targetedPuyaVersion: '4.4.0',
 } as const
 
 export type SupportedAvmVersion = (typeof Constants.supportedAvmVersions)[number]
