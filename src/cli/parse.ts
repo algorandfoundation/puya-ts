702215f8130e4732233a27dacaa805fe29cf5fc3
Tristan Menzel <tristanmenzel@gmail.com>
2025-03-18 12:14:05 -0700

    refactor: Remove need for explicit build command and default paths to current directory

--- src/cli/parse.ts
+++ src/cli/parse.ts
@@ -15,10 +15,10 @@ export async function parseCliArguments() {
     dest: 'command',
   })
   parser.set_defaults({
-    command: 'none',
+    command: 'build',
   })
-  addBuildCommand(parser.add_subparsers())
-  const result: PuyaTsCommand = parser.parse_args(getArgs())
+  addBuildCommand(parser)
+  const result: PuyaTsCommand = parser.parse_args()
   switch (result.command) {
     case 'build':
       await buildCommand(result)
@@ -31,31 +31,6 @@ export async function parseCliArguments() {
       parser.print_help()
       break
   }
-}
-
-/**
- * Return the args passed to the cli. This is argv minus the preamble of the interpreter and entry file.
- *
- * Injects a default command of `build` if none is present. If a new command is added, it will need to be injected below
- */
-function getArgs() {
-  // Get args minus the interpreter (ie. node.exe) and entry file (ie. cli.ts)
-  const args = process.argv.slice(2)
-  const [maybeCmd, ...rest] = args
-
-  switch (maybeCmd) {
-    case 'build':
-    case '--version':
-    case '--help':
-    case '-h':
-      break
-    default:
-      // Default to a build command
-      if (maybeCmd !== undefined) {
-        return ['build', maybeCmd, ...rest]
-      }
-  }
-  return args
 }
 
 type PuyaTsCommand = NoCommandArgs | BuildCommandArgs | VersionCommand
