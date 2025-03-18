7745fb8457c43f2d38d4dd04915f3cbf64de38d9
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-03-18 15:06:22 +1000

    chore: use lock-file

--- src/puya/resolve-puya-path.ts
+++ src/puya/resolve-puya-path.ts
@@ -1,3 +1,4 @@
+import * as lockfile from 'proper-lockfile'
 import { Constants } from '../constants'
 import { logger } from '../logger'
 import { downloadPuyaBinary, findCachedPuyaBinary } from './puya-binary'
@@ -6,12 +7,27 @@ import { parseSemVer } from './semver'
 export async function resolvePuyaPath(): Promise<string> {
   const version = parseSemVer(Constants.targetedPuyaVersion)
 
-  const cachedBinaryPath = findCachedPuyaBinary(version)
-  if (cachedBinaryPath) {
-    logger.debug(undefined, `Found cached Puya binary at ${cachedBinaryPath}`)
-    return cachedBinaryPath
-  }
+  const lockRelease = await lockfile.lock('foo', {
+    realpath: false,
+    stale: 30000, // Consider the lock stale after 30 seconds
+    retries: {
+      retries: 5,
+      factor: 2,
+      minTimeout: 1000,
+      maxTimeout: 30000,
+    },
+  })
 
-  const downloadedPath = await downloadPuyaBinary(version)
-  return downloadedPath
+  try {
+    const cachedBinaryPath = findCachedPuyaBinary(version)
+    if (cachedBinaryPath) {
+      logger.debug(undefined, `Found cached Puya binary at ${cachedBinaryPath}`)
+      return cachedBinaryPath
+    }
+
+    const downloadedPath = await downloadPuyaBinary(version)
+    return downloadedPath
+  } finally {
+    lockRelease()
+  }
 }
