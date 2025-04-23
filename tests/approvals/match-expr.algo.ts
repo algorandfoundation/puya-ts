1521801af0fbabad756c709bb514c5b21d583ddf
Tristan Menzel <tristanmenzel@gmail.com>
2025-04-22 18:57:34 -0700

    feat: Support `not` expressions on `match` and `assertMatch`

--- tests/approvals/match-expr.algo.ts
+++ tests/approvals/match-expr.algo.ts
@@ -1,11 +1,12 @@
 import type { uint64 } from '@algorandfoundation/algorand-typescript'
-import { assert, assertMatch, Contract, match } from '@algorandfoundation/algorand-typescript'
+import { assert, assertMatch, Contract, Global, match, Txn } from '@algorandfoundation/algorand-typescript'
 
 class MatchExprAlgo extends Contract {
   public testMatches(x: uint64) {
     const xObj = { x }
     assertMatch(xObj, { x: 5 }, 'x should be 5')
-
+    assertMatch(xObj, { x: { not: 3 } }, 'x should not be 3')
+    assertMatch(Txn, { sender: { not: Global.zeroAddress } })
     assert(match(xObj, { x: { greaterThan: 4 } }))
     assert(match(xObj, { x: { lessThan: 6 } }))
     assert(match(xObj, { x: { greaterThanEq: 5 } }))
