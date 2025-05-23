e9f7b22e1f11da47d168913db1078f7f86486c84
Bobby Lat <bobby.lat@makerx.com.au>
2025-05-23 10:49:04 +0800

    refactor: create a base class to share between MutableObjectVisitor and StructVisitor

--- src/awst_build/ast-visitors/struct-visitor.ts
+++ src/awst_build/ast-visitors/struct-visitor.ts
@@ -1,17 +1,11 @@
 import type ts from 'typescript'
-import { CodeError } from '../../errors'
-import { logger } from '../../logger'
-import { invariant } from '../../util'
 import type { ClassElements } from '../../visitor/syntax-names'
 import type { Visitor } from '../../visitor/visitor'
-import { accept } from '../../visitor/visitor'
 import { AwstBuildContext } from '../context/awst-build-context'
 import type { ARC4StructType } from '../ptypes/arc4-types'
-import { BaseVisitor } from './base-visitor'
+import { ClassDefinitionVisitor } from './class-definition-visitor'
 
-export class StructVisitor extends BaseVisitor implements Visitor<ClassElements, void> {
-  public accept = <TNode extends ts.Node>(node: TNode) => accept<StructVisitor, TNode>(this, node)
-
+export class StructVisitor extends ClassDefinitionVisitor implements Visitor<ClassElements, void> {
   static buildStructDef(classDec: ts.ClassDeclaration, ptype: ARC4StructType) {
     return AwstBuildContext.current.runInChildContext(() => {
       new StructVisitor(classDec, ptype)
@@ -20,46 +14,6 @@ export class StructVisitor extends BaseVisitor implements Visitor<ClassElements,
   }
 
   constructor(classDec: ts.ClassDeclaration, ptype: ARC4StructType) {
-    super()
-
-    for (const member of classDec.members) {
-      try {
-        this.accept(member)
-      } catch (e) {
-        invariant(e instanceof Error, 'Only errors should be thrown')
-        logger.error(e)
-      }
-    }
-  }
-
-  private throwStructNotSupported(node: ts.Node, desc: string): never {
-    throw new CodeError(`${desc} are not supported in ARC4 struct definitions`, {
-      sourceLocation: this.sourceLocation(node),
-    })
-  }
-
-  visitClassStaticBlockDeclaration(node: ts.ClassStaticBlockDeclaration) {
-    this.throwStructNotSupported(node, 'Class static block declarations')
-  }
-  visitConstructor(node: ts.ConstructorDeclaration) {
-    this.throwStructNotSupported(node, 'Constructor declarations')
-  }
-  visitGetAccessor(node: ts.GetAccessorDeclaration) {
-    this.throwStructNotSupported(node, 'Property declarations')
-  }
-  visitIndexSignature(node: ts.IndexSignatureDeclaration) {
-    this.throwStructNotSupported(node, 'Index signature declarations')
-  }
-  visitMethodDeclaration(node: ts.MethodDeclaration) {
-    this.throwStructNotSupported(node, 'Method declarations')
-  }
-  visitPropertyDeclaration(node: ts.PropertyDeclaration) {
-    this.throwStructNotSupported(node, 'Property declarations')
-  }
-  visitSemicolonClassElement(node: ts.SemicolonClassElement) {
-    // Ignore
-  }
-  visitSetAccessor(node: ts.SetAccessorDeclaration) {
-    this.throwStructNotSupported(node, 'Property declarations')
+    super(classDec)
   }
 }
