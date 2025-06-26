c202e50f87176305221225e9c60637dd4b238326
Tristan Menzel <tristanmenzel@gmail.com>
2025-06-26 15:59:59 -0700

    Increase test coverage of assignments

--- tests/approvals/assignments.algo.ts
+++ tests/approvals/assignments.algo.ts
@@ -1,30 +1,59 @@
 import type { uint64 } from '@algorandfoundation/algorand-typescript'
-import { assert, clone, Contract, log } from '@algorandfoundation/algorand-typescript'
+import { assert, assertMatch, clone, contract, Contract, log, op } from '@algorandfoundation/algorand-typescript'
 
 type MutableObj = { a: uint64; b: uint64 }
 type ImmutableObj = { readonly a: uint64; readonly b: uint64 }
 
+const countSlot = 0
+function resetCount() {
+  op.Scratch.store(countSlot, 0)
+}
+function getCount() {
+  return op.Scratch.loadUint64(countSlot)
+}
+
+function incCount() {
+  op.Scratch.store(countSlot, op.Scratch.loadUint64(countSlot) + 1)
+}
+
+@contract({ scratchSlots: [countSlot] })
 export class AssignmentsAlgo extends Contract {
   testPrimitives(u: uint64) {
     const p1 = u
     let p2: uint64 = 2
     p2 += 1
+    assert(p2 === 3)
+    assert(p1 === u)
   }
 
   testArrayDestructure(i_a: ReadonlyArray<uint64>, u: uint64, m_a: uint64[]) {
     const [, a1] = i_a
+    assert(a1 === i_a[1])
     let a2: uint64, a3: uint64
+    resetCount()
     const a5 = ([a2, a3] = [logAndReturn(u), logAndReturn(u), logAndReturn(u)])
+    assert(getCount() === 3, 'logAndReturn called 3 times')
+    assert(a2 === u)
+    assert(a3 === u)
+    assertMatch(a5, [u, u, u])
     assert(a5.length === 3, 'a5 length should be 3')
     const [a4]: [uint64] = [1]
+    assert(a4 === 1)
     const [c, d] = [logAndReturn(u), logAndReturn(u), logAndReturn(u)]
+    assertMatch([c, d], [u, u])
     const [a6, a7] = m_a
+    assert(a6 === m_a[0])
+    assert(a7 === m_a[1])
   }
 
   testArrayNarrowing(m_a: uint64[], u: uint64) {
     const direct = clone(m_a)
+    m_a[0] += 1
+    direct[0] += 2
+    assert(m_a[0] !== direct[0])
 
     const narrowed: readonly uint64[] = clone(m_a)
+    const narrowed2 = narrowed
     let fromLit: readonly uint64[]
     const result = (fromLit = [u, u, u])
 
@@ -33,6 +62,7 @@ export class AssignmentsAlgo extends Contract {
 
   testNested(i_a: ReadonlyArray<ReadonlyArray<uint64>>) {
     const [[a]] = i_a
+    assert(a === i_a[0][0])
   }
 
   testDestructureObj(m: { a: uint64; b: string }, i: Readonly<{ a: uint64; b: string }>) {
@@ -72,6 +102,7 @@ function getVal(): ImmutableObj {
 }
 
 function logAndReturn(u: uint64): uint64 {
+  incCount()
   log(u)
   return u
 }
--- tests/expected-output/mutating-tuples.algo.ts
+++ tests/expected-output/mutating-tuples.algo.ts
@@ -1,10 +1,12 @@
+// noinspection JSConstantReassignment
+
 import type { uint64 } from '@algorandfoundation/algorand-typescript'
 
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 function test(a: uint64) {
   const t1: readonly [uint64, uint64, uint64] = [a, a, a]
-  let t2 = { a, b: a, c: a }
+  let t2 = { a, b: a, c: a } as const
 
   a *= 2
 
@@ -13,6 +15,7 @@ function test(a: uint64) {
   t1[0] = a
 
   // @expect-error Expression is not a valid assignment target - object is immutable
+  // @ts-expect-error
   t2.a = a
 
   t2 = { ...t2, a }
