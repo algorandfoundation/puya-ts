e9cf9c41f6665b9fdc3ed80f21e6c0ca0fb73d84
Bobby Lat <bobby.lat@makerx.com.au>
2025-07-02 16:13:10 +0700

    remove copy function from FixedArray stub

--- tests/approvals/arc4-clone.algo.ts
+++ tests/approvals/arc4-clone.algo.ts
@@ -1,47 +1,70 @@
 import type { uint64 } from '@algorandfoundation/algorand-typescript'
-import { Contract, Uint64 } from '@algorandfoundation/algorand-typescript'
+import { arc4, assert, clone, Contract, Uint64 } from '@algorandfoundation/algorand-typescript'
+
+// This struct is shared
+class SharedStruct extends arc4.Struct<{
+  foo: arc4.DynamicBytes
+  bar: arc4.UintN8
+}> {}
+
+class TopLevelStruct extends arc4.Struct<{
+  a: arc4.UintN64
+  b: arc4.Str
+  shared: SharedStruct
+}> {}
 
 export class Arc4CloneAlgo extends Contract {
   aliasing(mutable: uint64[]) {
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being assigned to another variable
-    const needClone = mutable
+    const needClone = clone(mutable)
 
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being assigned to another variable
-    const needClone2: readonly uint64[] = mutable
+    const needClone2: readonly uint64[] = clone(mutable)
 
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being passed to a tuple expression
-    const tupleOfMutable: readonly [uint64[], uint64] = [mutable, 1]
+    const tupleOfMutable: readonly [uint64[], uint64] = [clone(mutable), 1]
 
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being passed to a tuple expression
-    const [x] = tupleOfMutable
+    const [x] = clone(tupleOfMutable)
 
     const mutableTupleOfMutable: [uint64[]] = [[1, 2, 3]]
 
     // Ideally this would be the same error as above, but because we translate mutable destructuring into comma expressions it's impossible to reliably
     // detect that the original expression was a destructuring one, from the AWST alone.
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being passed to a tuple expression
-    const [y] = mutableTupleOfMutable
+    const [y] = clone(mutableTupleOfMutable)
     let z: uint64[]
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being assigned to another variable
-    this.receive((z = mutable))
+    this.receive((z = clone(mutable)))
+
+    this.receiveReadonly(mutable)
 
     // This is fine since spread is a shallow clone
     const m2 = [...mutable]
 
     const nestedMutables = [[Uint64(1)]]
 
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being passed to a tuple expression
-    const nestedMutables2 = [mutable, [Uint64(2)]]
+    const nestedMutables2 = [clone(mutable), [Uint64(2)]]
 
-    // @expect-error cannot create multiple references to a mutable stack type, the value must be copied using clone(...) when being assigned to another variable
-    const m3 = [...nestedMutables]
+    const m3 = [...clone(nestedMutables)]
 
-    // @expect-error expression containing a reference to value with nested mutable types must be cloned when being concatenated
-    const m4 = nestedMutables.concat([Uint64(123)])
+    const m4 = clone(nestedMutables).concat([Uint64(123)])
 
     // This is fine because there are no nested mutables
     const m5 = mutable.concat(mutable)
   }
 
   receive(mutable: uint64[]) {}
+
+  receiveReadonly(a: readonly uint64[]) {}
+
+  structReturn(arg: TopLevelStruct): SharedStruct {
+    // this throws an error in python and requires `_HasAssignmentVisitor.check(expr.condition)` in arc4 copy validator
+    assert(arg.shared === echo(arg.shared), "this won't error")
+    return arg.shared
+  }
 }
+
+function echo(s: SharedStruct): SharedStruct {
+  return s
+}
+/*
+
+
+./TELUS-606088783-2024-01-13.pdf ./TELUS-606088783-2024-02-13.pdf ./TELUS-606088783-2024-03-13.pdf ./TELUS-606088783-2024-04-13.pdf ./TELUS-606088783-2024-05-13.pdf ./TELUS-606088783-2024-06-13.pdf ./TELUS-606088783-2024-07-13.pdf ./TELUS-606088783-2024-08-13.pdf ./TELUS-606088783-2024-09-13.pdf ./TELUS-606088783-2024-10-13.pdf ./TELUS-606088783-2024-11-13.pdf ./TELUS-606088783-2024-12-13.pdf
+
+ */
