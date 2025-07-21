92802b768375f8986ef1f02d74be8d972965c68c
Adam Chidlow <achidlow@users.noreply.github.com>
2025-07-21 17:19:10 +0800

    ingore missing .env file when running tests

--- test.setup.ts
+++ test.setup.ts
@@ -5,4 +5,7 @@ import path from 'path'
 // Set max listeners to infinite for tests since vitest makes use of listeners per running test to detect errors
 process.setMaxListeners(0)
 
-dotenv.config({ path: path.join(__dirname, '.env') })
+dotenv.config({
+  path: path.join(__dirname, '.env'),
+  ignore: ['MISSING_ENV_FILE'],
+})
