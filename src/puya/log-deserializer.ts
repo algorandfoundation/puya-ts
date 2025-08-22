891ec0a477da266be96bf562955fcd91e4114eaa
Tristan Menzel <tristanmenzel@gmail.com>
2025-08-22 14:59:57 -0700

    refactor: Remove zod and rxjs dependencies

--- src/puya/log-deserializer.ts
+++ src/puya/log-deserializer.ts
@@ -1,25 +1,58 @@
 import upath from 'upath'
-import { z } from 'zod'
 import { SourceLocation } from '../awst/source-location'
 import { logger, LogLevel, LogSource } from '../logger'
+import { invariant } from '../util'
 
-const puyaLog = z.object({
-  level: z.nativeEnum(LogLevel),
-  location: z
-    .object({
-      file: z.string(),
-      line: z.number(),
-      end_line: z.number().or(z.null()),
-      column: z.number(),
-      end_column: z.number().or(z.null()),
-    })
-    .or(z.null()),
-  message: z.string(),
-})
+function parseString(x: unknown) {
+  invariant(typeof x === 'string', `expected string, received ${typeof x}`)
+  return x
+}
+function parseNumber(x: unknown) {
+  invariant(typeof x === 'number', `expected number, received ${typeof x}`)
+  return x
+}
+function parseNullOrNumber(x: unknown) {
+  if (x === null) return null
+  return parseNumber(x)
+}
+function parseLogLevel(x: unknown) {
+  switch (x) {
+    case LogLevel.Error:
+    case LogLevel.Info:
+    case LogLevel.Warning:
+    case LogLevel.Debug:
+    case LogLevel.Critical:
+      return x
+    default:
+      invariant(false, `expected LogLevel, received ${x}`)
+  }
+}
+function parseSourceLocation(x: unknown) {
+  invariant(typeof x === 'object', `expected object, received ${typeof x}`)
+  if (x === null) return null
+  const obj = x as Record<string, unknown>
+  return {
+    file: parseString(obj.file),
+    line: parseNumber(obj.line),
+    end_line: parseNullOrNumber(obj.end_line),
+    column: parseNumber(obj.column),
+    end_column: parseNullOrNumber(obj.end_column),
+  }
+}
+
+function parsePuyaLog(x: unknown) {
+  invariant(typeof x === 'object' && x !== null, `expected object, received ${typeof x}`)
+  const obj = x as Record<string, unknown>
+  return {
+    level: parseLogLevel(obj.level),
+    message: parseString(obj.message),
+    location: parseSourceLocation(obj.location),
+  }
+}
 
 export function deserializeAndLog(logText: string) {
   try {
-    const log = puyaLog.parse(JSON.parse(logText))
+    const log = parsePuyaLog(JSON.parse(logText))
 
     const sourceLocation = log.location
       ? new SourceLocation({
