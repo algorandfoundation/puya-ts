d4ddea0a11628488fe74dab3b3a092bb465ae696
Bobby Lat <bobby.lat@makerx.com.au>
2025-10-20 13:44:43 +0800

    log puya-ts and puya versions when compiling

--- src/cli/app-version.ts
+++ src/cli/app-version.ts
@@ -1,22 +1,24 @@
-import fs from 'fs'
-import { fileURLToPath } from 'node:url'
-import upath from 'upath'
-import { Constants } from '../constants'
+import fs from 'fs';
+import { fileURLToPath } from 'node:url';
+import upath from 'upath';
+import { Constants } from '../constants';
 
-export function appVersion(name: string = 'puya-ts') {
+export function appVersion(options?: { name?: string; withAVMVersion?: boolean }): string {
   let dirName = upath.dirname(fileURLToPath(import.meta.url))
+  const name = options?.name ?? 'puya-ts'
+  const withAVMVersion = options?.withAVMVersion ?? true
 
   while (true) {
     const packageJsonPath = upath.join(dirName, 'package.json')
     if (fs.existsSync(packageJsonPath)) {
       const version = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8')).version
-      return [
-        `${name} ${version}`,
-        '',
-        'Targets:',
-        `puya ${Constants.targetedPuyaVersion}`,
-        `AVM ${Constants.supportedAvmVersions.join(', ')}`,
-      ].join('\r\n')
+      return [`${name} ${version}`]
+        .concat(
+          withAVMVersion
+            ? ['', 'Targets:', `puya ${Constants.targetedPuyaVersion}`, `AVM ${Constants.supportedAvmVersions.join(', ')}`]
+            : [`puya ${Constants.targetedPuyaVersion}`],
+        )
+        .join('\r\n')
     }
     if (dirName === upath.dirname(dirName)) break
     dirName = upath.dirname(dirName)
