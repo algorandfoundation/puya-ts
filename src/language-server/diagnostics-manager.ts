475189d029949213138225803dcf511a926f4309
Tristan Menzel <tristan.menzel@makerx.com.au>
2025-10-28 13:56:33 +1000

    refactor: Use pending status instead of empty diagnostics to indicate that a new file version is awaiting diagnostics

--- src/language-server/diagnostics-manager.ts
+++ src/language-server/diagnostics-manager.ts
@@ -21,17 +21,17 @@ function isSameOrMoreRecentVersion(prev: number | undefined, next: number | unde
 }
 
 export class SourceFileDiagnostics {
-  private diagnostics: lsp.Diagnostic[] | undefined = undefined
+  private diagnostics: lsp.Diagnostic[] | 'pending' = 'pending'
   private version: number | undefined
   /**
    * Update the diagnostics for this file and return a boolean indicating if they have changed
    * @param version The file version used to generate these diagnostics
    * @param diagnostics
    */
-  setDiagnostics({ version, diagnostics }: { diagnostics: lsp.Diagnostic[]; version: number | undefined }): boolean {
+  setDiagnostics({ version, diagnostics }: { diagnostics: lsp.Diagnostic[] | 'pending'; version: number | undefined }): boolean {
     // Ignore older diagnostics (even though this shouldn't happen)
     if (!isSameOrMoreRecentVersion(this.version, version)) return false
-    if (diagnostics.length === this.diagnostics?.length) {
+    if (diagnostics !== 'pending' && this.diagnostics !== 'pending' && diagnostics.length === this.diagnostics.length) {
       if (zipStrict(this.diagnostics, diagnostics).every(([l, r]) => diagnosticsAreSame(l, r))) {
         this.version = version
         return false
@@ -63,12 +63,16 @@ export class DiagnosticsManager {
     version,
   }: {
     fileUri: lsp.DocumentUri
-    diagnostics: lsp.Diagnostic[]
+    diagnostics: lsp.Diagnostic[] | 'pending'
     version: number | undefined
   }) {
-    logger.debug(undefined, `[DiagMgr] Setting diagnostics for ${fileUri} ${version ?? '<no version>'}`)
+    logger.debug(
+      undefined,
+      `[DiagMgr] Setting diagnostics for ${fileUri} ${version ?? '<no version>'} (${diagnostics === 'pending' ? 'pending' : diagnostics.length})`,
+    )
     const fileDiagnostics = this.sourceFiles.getOrDefault(fileUri, () => new SourceFileDiagnostics())
     if (fileDiagnostics.setDiagnostics({ diagnostics, version })) {
+      if (diagnostics === 'pending') return
       this.events.emit('fileDiagnosticsChanged', { uri: fileUri, diagnostics, version })
     }
   }
