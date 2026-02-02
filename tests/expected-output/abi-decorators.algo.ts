d194fcb2e74bdbf863e82fbc09e22be86c581bbf
Bobby Lat <bobby.lat@makerx.com.au>
2026-02-02 14:21:12 +0700

    refactor: let arc-4 method arg type check be done in ir layer

--- tests/expected-output/abi-decorators.algo.ts
+++ tests/expected-output/abi-decorators.algo.ts
@@ -1,5 +1,5 @@
-import type { bytes, gtxn, itxn, uint64 } from '@algorandfoundation/algorand-typescript'
-import { abimethod, baremethod, Contract, err, GlobalState, readonly, Uint64 } from '@algorandfoundation/algorand-typescript'
+import type { bytes, uint64 } from '@algorandfoundation/algorand-typescript'
+import { abimethod, baremethod, Contract, GlobalState, readonly, Uint64 } from '@algorandfoundation/algorand-typescript'
 
 export default class AbiDecorators extends Contract {
   // @expect-warning Duplicate on completion actions
@@ -18,16 +18,6 @@ export default class AbiDecorators extends Contract {
   // @expect-error Default argument specification for 'a' does not match parameter type
   @abimethod({ defaultArguments: { a: { from: 'globalValue' } } })
   public methodWithDefaults(a: bytes): void {}
-
-  // @expect-error PaymentTxn cannot be used as an ABI return type
-  badReturnsGtxn(): gtxn.PaymentTxn {
-    err('No implementation')
-  }
-
-  // @expect-error PaymentInnerTxn cannot be used as an ABI return type
-  badReturnsItxn(): itxn.PaymentInnerTxn {
-    err('No implementation')
-  }
 
   // @expect-error Bare methods cannot have any parameters
   @baremethod()
