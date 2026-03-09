518d98c60a452bf0e6980f1ffa23fa9abe67c015
Bobby Lat <bobby.lat@makerx.com.au>
2026-03-09 14:56:52 +0800

    refactor: stop parsing event signature string to simplify frontend

--- tests/expected-output/arc28-events.algo.ts
+++ tests/expected-output/arc28-events.algo.ts
@@ -5,12 +5,6 @@ class EventEmitter extends Contract {
   emitSwapped(a: uint64, b: uint64) {
     // @expect-error Event cannot be an anonymous type...
     emit({ a: b, b: a })
-
-    // @expect-error Event signature length (2) does not match number of provided values (1).
-    emit('Swapped(uint64,uint64)', b)
-
-    // @expect-error Invalid signature: Tuple has not been closed
-    emit('Swapped(uint64', b)
   }
 
   emitCustom(arg0: string, arg1: boolean) {
