80f2f030e1c18ef002b96e5a989606e61412f1d5
Bobby Lat <bobby.lat@makerx.com.au>
2026-03-09 14:56:52 +0800

    refactor: stop parsing event signature string to simplify frontend

--- src/awst_build/eb/arc28/arc-28-emit-function-builder.ts
+++ src/awst_build/eb/arc28/arc-28-emit-function-builder.ts
@@ -1,15 +1,13 @@
 import type ts from 'typescript'
 import { nodeFactory } from '../../../awst/node-factory'
-import type { Expression, StringConstant } from '../../../awst/nodes'
-import { SourceLocation } from '../../../awst/source-location'
-import { CodeError, InternalError } from '../../../errors'
+import type { Expression } from '../../../awst/nodes'
+import type { SourceLocation } from '../../../awst/source-location'
+import { InternalError } from '../../../errors'
 import { logger } from '../../../logger'
 import { codeInvariant, instanceOfAny } from '../../../util'
-import { Arc4ParseError, parseArc4Type } from '../../../util/arc4-signature-parser'
 import type { PType } from '../../ptypes'
 import { arc28EmitFunction, ArrayLiteralPType, ImmutableObjectPType, MutableObjectPType, stringPType, voidPType } from '../../ptypes'
-import type { ARC4EncodedType } from '../../ptypes/arc4-types'
-import { ARC4StructType, ARC4TupleType } from '../../ptypes/arc4-types'
+import { ARC4StructType } from '../../ptypes/arc4-types'
 import { instanceEb } from '../../type-registry'
 import type { InstanceBuilder, NodeBuilder } from '../index'
 import { FunctionBuilder } from '../index'
@@ -19,19 +17,12 @@ import { parseFunctionArgs } from '../util/arg-parsing'
 export class Arc28EmitFunctionBuilder extends FunctionBuilder {
   readonly ptype = arc28EmitFunction
 
-  private encode(builder: InstanceBuilder, encodedType: ARC4EncodedType) {
-    return nodeFactory.aRC4Encode({
-      value: builder.resolve(),
-      sourceLocation: builder.sourceLocation,
-      wtype: encodedType.wtype,
-    })
-  }
-
-  private resolvePropertyValue(builder: InstanceBuilder, expectedType?: ARC4EncodedType): [Expression, PType] {
-    if (builder.ptype instanceof ArrayLiteralPType && expectedType) {
-      return [this.encode(builder.resolveToPType(builder.ptype.getReadonlyTupleType()), expectedType), expectedType]
+  private resolvePropertyValue(builder: InstanceBuilder): Expression {
+    if (builder.ptype instanceof ArrayLiteralPType) {
+      const tupleType = builder.ptype.getReadonlyTupleType()
+      return builder.resolveToPType(tupleType).resolve()
     }
-    return [builder.resolve(), builder.ptype]
+    return builder.resolve()
   }
 
   call(args: ReadonlyArray<NodeBuilder>, typeArgs: ReadonlyArray<PType>, sourceLocation: SourceLocation<ts.CallExpression>): NodeBuilder {
@@ -49,20 +40,8 @@ export class Arc28EmitFunctionBuilder extends FunctionBuilder {
 
     if (nameOrObj.ptype.equals(stringPType)) {
       const signature = requireStringConstant(nameOrObj)
-      const { name: _, propTypes } = parseEventName(nameOrObj)
 
-      const values: Expression[] = []
-      for (const [index, prop] of props.entries()) {
-        const [encodedExpr, _] = this.resolvePropertyValue(prop, propTypes?.[index])
-        values.push(encodedExpr)
-      }
-
-      if (propTypes && propTypes.length !== values.length) {
-        throw new CodeError(`Event signature length (${propTypes.length}) does not match number of provided values (${values.length}).`, {
-          sourceLocation: sourceLocation,
-        })
-      }
-
+      const values = props.map((p) => this.resolvePropertyValue(p))
       return instanceEb(
         nodeFactory.emitFields({
           signature: signature.value,
@@ -103,42 +82,4 @@ function emitStruct(expression: Expression, sourceLocation: SourceLocation) {
     }),
     voidPType,
   )
-}
-
-function parseEventName(nameBuilder: InstanceBuilder): {
-  name: StringConstant
-  propTypes?: ARC4EncodedType[]
-} {
-  const name = requireStringConstant(nameBuilder)
-  const parenthesisIndex = name.value.indexOf('(')
-  if (parenthesisIndex === -1) {
-    return {
-      name,
-    }
-  }
-  const signature = name.value.substring(parenthesisIndex)
-
-  try {
-    const signatureType = parseArc4Type(signature)
-    codeInvariant(signatureType instanceof ARC4TupleType, 'Event signature must be a tuple type', name.sourceLocation)
-    return {
-      name: nodeFactory.stringConstant({
-        value: name.value.substring(0, parenthesisIndex),
-        sourceLocation: name.sourceLocation,
-      }),
-      propTypes: signatureType.items,
-    }
-  } catch (e) {
-    if (e instanceof Arc4ParseError) {
-      // Source location adjustment assumes StringConstant is all on one line
-      throw new CodeError(`Invalid signature: ${e.message}`, {
-        sourceLocation: new SourceLocation({
-          ...name.sourceLocation,
-          column: name.sourceLocation.column + parenthesisIndex + e.index,
-        }),
-      })
-    } else {
-      throw e
-    }
-  }
 }
