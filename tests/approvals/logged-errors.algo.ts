e19ee5b5fd28d05247b287e9ae3c8388d2dfd762
ArgimiroCF <argimirodelpozo@gmail.com>
2026-03-10 12:04:19 -0300

    test: add logged-errors and backend warning excercising tests

---
+++ tests/approvals/logged-errors.algo.ts
@@ -0,0 +1,23 @@
+import type { uint64 } from '@algorandfoundation/algorand-typescript'
+import { Contract, loggedAssert, loggedErr } from '@algorandfoundation/algorand-typescript'
+
+class LoggedErrorsValidContract extends Contract {
+  public testValid(arg: uint64): void {
+    loggedAssert(arg !== 1, '01')
+    loggedAssert(arg !== 2, '02', 'arg is two')
+    loggedAssert(arg !== 3, '03', undefined, 'AER')
+    loggedAssert(arg !== 4, '04', 'arg is 4', 'AER')
+    if (arg === 5) {
+      loggedErr('05')
+    }
+    if (arg === 6) {
+      loggedErr('06', 'arg was 6')
+    }
+    if (arg === 7) {
+      loggedErr('07', undefined, 'AER')
+    }
+    if (arg === 8) {
+      loggedErr('08', 'arg is eight (08)', 'AER')
+    }
+  }
+}
