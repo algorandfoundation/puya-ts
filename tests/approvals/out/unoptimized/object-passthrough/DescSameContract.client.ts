ece4d4f5f064c21e771b0b7411e27971ac957eed
ArgimiroCF <argimirodelpozo@gmail.com>
2026-04-23 19:22:55 -0300

    chore: build

---
+++ tests/approvals/out/unoptimized/object-passthrough/DescSameContract.client.ts
@@ -0,0 +1,14 @@
+// This file is auto-generated, do not modify
+/* eslint-disable */
+import { Contract, abimethod, arc4, err } from '@algorandfoundation/algorand-typescript'
+
+export class Object2E8A71EC extends arc4.Struct<{
+  x: arc4.Uint<64>
+}> {}
+
+export abstract class DescSameContract extends Contract {
+  @abimethod()
+  forward(a: Object2E8A71EC): Object2E8A71EC {
+    err('stub only')
+  }
+}
