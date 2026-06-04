cab813dc4901625e9e3cd45d99a0d983d95f4840
ArgimiroCF <argimirodelpozo@gmail.com>
2026-06-04 15:22:08 -0300

    test: added arc4 routing test cases to prevent routing bug regression

--- tests/onchain/arc4-hybrid.spec.ts
+++ tests/onchain/arc4-hybrid.spec.ts
@@ -4,10 +4,18 @@ import { createArc4TestFixture } from './util/test-fixture'
 
 describe('arc4-hybrid', () => {
   const test = createArc4TestFixture({ paths: 'tests/approvals/arc4-hybrid.algo.ts', contracts: { Arc4HybridAlgo: {} } })
-  test('works as expected', async ({ appClientArc4HybridAlgo }) => {
-    const result = await appClientArc4HybridAlgo.send.call({ method: 'someMethod' })
+  test('routes abi methods', async ({ appClientArc4HybridAlgo }) => {
+    const someMethod = await appClientArc4HybridAlgo.send.call({ method: 'someMethod' })
+    expect(someMethod.confirmation.logs!.map(uint8ArrayToUtf8)).toStrictEqual(['before', 'some method', 'after'])
+
+    const someOtherMethod = await appClientArc4HybridAlgo.send.call({ method: 'someOtherMethod' })
+    expect(someOtherMethod.confirmation.logs!.map(uint8ArrayToUtf8)).toStrictEqual(['before', 'some other method', 'after'])
+  })
+
+  test('handles update calls', async ({ appClientArc4HybridAlgo }) => {
+    const result = await appClientArc4HybridAlgo.send.update({ method: 'updateApplication' })
     const logs = result.confirmation.logs!.map(uint8ArrayToUtf8)
 
-    expect(logs).toStrictEqual(['before', 'some method', 'after'])
+    expect(logs).toStrictEqual(['before', 'update', 'after'])
   })
 })
