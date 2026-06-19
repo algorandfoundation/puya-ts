51b7cf19baa115de6c04ed573d8a06ce4f44ca24
ArgimiroCF <argimirodelpozo@gmail.com>
2026-06-19 01:48:51 -0300

    test: add tests for recursive function and recursive type where the actual type differs on expansion

--- tests/expected-output/recursive-types.algo.ts
+++ tests/expected-output/recursive-types.algo.ts
@@ -1,5 +1,21 @@
 import type { DynamicArray } from '@algorandfoundation/algorand-typescript/arc4'
-import { Struct } from '@algorandfoundation/algorand-typescript/arc4'
+import { Bool, Struct } from '@algorandfoundation/algorand-typescript/arc4'
 
-// @expect-error type is part of a cyclic reference
+// @expect-error type 'A' is part of a cyclic reference
 class A extends Struct<{ x: DynamicArray<A> }> {}
+
+type Rec = () => Rec
+// @expect-error type 'Rec' is part of a cyclic reference
+export function foo(f: Rec) {
+  return f
+}
+
+type F<T> = () => F<[T]>
+// @expect-error type 'F' is part of a cyclic reference
+export function bar(f: F<boolean>) {
+  return f
+}
+
+// @expect-error type 'B' is part of a cyclic reference
+class B<T> extends Struct<{ x: B<[T]> }> {}
+class C extends B<Bool> {}
