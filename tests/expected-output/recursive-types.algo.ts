0073561a5d9673298d1ef3a23e4c2b6098dc09e9
ArgimiroCF <argimirodelpozo@gmail.com>
2026-06-19 11:11:38 -0300

    chore: fix linter issue

--- tests/expected-output/recursive-types.algo.ts
+++ tests/expected-output/recursive-types.algo.ts
@@ -1,5 +1,5 @@
-import type { DynamicArray } from '@algorandfoundation/algorand-typescript/arc4'
-import { Bool, Struct } from '@algorandfoundation/algorand-typescript/arc4'
+import type { DynamicArray, Bool } from '@algorandfoundation/algorand-typescript/arc4'
+import { Struct } from '@algorandfoundation/algorand-typescript/arc4'
 
 // @expect-error type 'A' is part of a cyclic reference
 class A extends Struct<{ x: DynamicArray<A> }> {}
